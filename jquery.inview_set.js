b1d7340b8f5003be49455fdf3495ab7cfecfbc70
komoro1215 <s_komoro2007@yahoo.co.jp>
2026-02-15 22:32:49 +0900

    Create jquery.inview_set.js

---
+++ jquery.inview_set.js
@@ -0,0 +1,30 @@
+
+//up繧ｹ繧ｿ繧､繝ｫ縺檎判髱｢蜀�↓縺阪◆繧峨√せ繧ｿ繧､繝ｫupstyle繧帝←逕ｨ縺吶ｋ
+$('.up').on('inview', function() {
+	$(this).addClass('upstyle');
+});
+
+//down繧ｹ繧ｿ繧､繝ｫ縺檎判髱｢蜀�↓縺阪◆繧峨√せ繧ｿ繧､繝ｫdownstyle繧帝←逕ｨ縺吶ｋ
+$('.down').on('inview', function() {
+	$(this).addClass('downstyle');
+});
+
+//transform1繧ｹ繧ｿ繧､繝ｫ縺檎判髱｢蜀�↓縺阪◆繧峨√せ繧ｿ繧､繝ｫtransform1style繧帝←逕ｨ縺吶ｋ
+$('.transform1').on('inview', function() {
+	$(this).addClass('transform1style');
+});
+
+//transform2繧ｹ繧ｿ繧､繝ｫ縺檎判髱｢蜀�↓縺阪◆繧峨√せ繧ｿ繧､繝ｫtransform2style繧帝←逕ｨ縺吶ｋ
+$('.transform2').on('inview', function() {
+	$(this).addClass('transform2style');
+});
+
+//transform3繧ｹ繧ｿ繧､繝ｫ縺檎判髱｢蜀�↓縺阪◆繧峨√せ繧ｿ繧､繝ｫtransform3style繧帝←逕ｨ縺吶ｋ
+$('.transform3').on('inview', function() {
+	$(this).addClass('transform3style');
+});
+
+//blur繧ｹ繧ｿ繧､繝ｫ縺檎判髱｢蜀�↓縺阪◆繧峨√せ繧ｿ繧､繝ｫblurstyle繧帝←逕ｨ縺吶ｋ
+$('.blur').on('inview', function() {
+	$(this).addClass('blurstyle');
+});
